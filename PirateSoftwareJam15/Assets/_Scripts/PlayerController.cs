5c0c0687b1dd2e5854ad842ef8ecde8aeb899bbb
CalvinDering <calvin.dering@gmail.com>
2024-07-26 00:02:48 +0200

    Added flashlight. Added volumetric light.

--- PirateSoftwareJam15/Assets/_Scripts/PlayerController.cs
+++ PirateSoftwareJam15/Assets/_Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour {
 
@@ -13,6 +14,10 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float lookXLimit = 75.0f;
     [SerializeField] private Transform interactionPoint;
     [SerializeField] private float interactionRange;
+    [SerializeField] private Transform lightPoint;
+    [SerializeField] private float lightRadius;
+    [SerializeField] private float maxLightRange;
+    [SerializeField] private float lightAngle;
 
     private bool pauseMoving = false;
 
@@ -21,6 +26,10 @@ public class PlayerController : MonoBehaviour {
     private void Awake() {
         playerRB = GetComponent<Rigidbody>();
         inputHandler = InputHandler.Instance;
+    }
+
+    private void Update() {
+        CheckLight();
     }
 
     private void FixedUpdate() {
@@ -40,23 +49,53 @@ public class PlayerController : MonoBehaviour {
         transform.Rotate(Vector3.up * inputHandler.Look.x * lookSpeed);
     }
 
+    public void CheckLight() {
+        RaycastHit[] hits = ConeCastAll(lightPoint.position, lightRadius, lightPoint.forward, maxLightRange, lightAngle);
+        if(hits.Length > 0) {
+            hits.Where(c => c.collider.gameObject.TryGetComponent(out ILightable lightable)).ToList().ForEach(l => l.collider.GetComponent<ILightable>().Lighten());
+        }
+    }
+
     public void Interact() {
         Ray ray = new Ray(interactionPoint.position, interactionPoint.forward);
         if(Physics.Raycast(ray, out RaycastHit hit, interactionRange)) {
-            if(hit.collider.gameObject.TryGetComponent(out ILightable lightable)) {
-                lightable.Lighten();
-            }
-            /*if(hit.collider.gameObject.TryGetComponent(out IInteractable interactable)) {
+            if(hit.collider.gameObject.TryGetComponent(out IInteractable interactable)) {
                 interactable.Interact(gameObject);
 
                 if(hit.collider.gameObject.TryGetComponent(out NPC npc)) {
                     pauseMoving = !pauseMoving;
                 }
-            }*/
+            }
         }
+    }
+
+    // Credits to: https://github.com/walterellisfun/ConeCast/tree/master
+    private RaycastHit[] ConeCastAll(Vector3 origin, float maxRadius, Vector3 direction, float maxDistance, float coneAngle) {
+        RaycastHit[] sphereCastHits = Physics.SphereCastAll(origin - new Vector3(0, 0, maxRadius), maxRadius, direction, maxDistance);
+        List<RaycastHit> coneCastHitList = new List<RaycastHit>();
+
+        if(sphereCastHits.Length > 0) {
+            for(int i = 0; i < sphereCastHits.Length; i++) {
+                Vector3 hitPoint = sphereCastHits[i].point;
+                Vector3 directionToHit = hitPoint - origin;
+                float angleToHit = Vector3.Angle(direction, directionToHit);
+
+                if(angleToHit < coneAngle) {
+                    coneCastHitList.Add(sphereCastHits[i]);
+                }
+            }
+        }
+
+        RaycastHit[] coneCastHits = new RaycastHit[coneCastHitList.Count];
+        coneCastHits = coneCastHitList.ToArray();
+
+        return coneCastHits;
+
     }
 
     private void OnDrawGizmosSelected() {
         Gizmos.DrawLine(transform.position, transform.position + transform.forward * interactionRange);
+
+        Gizmos.DrawWireSphere(lightPoint.position + lightPoint.forward * maxLightRange, lightRadius);
     }
 }
