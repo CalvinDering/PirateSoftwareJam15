d2a15b8657aa54b92a0923859eede9ca29d9cebb
CalvinDering <calvin.dering@gmail.com>
2024-07-30 11:08:34 +0200

    Added musicians to intro scene

---
+++ PirateSoftwareJam15/Assets/_Scripts/NPC/IntroMusician.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroMusician : MonoBehaviour {
+
+    private Animator animator;
+
+    [SerializeField] private float movementCooldown = 5f;
+
+    private float timer;
+
+    private void Awake() {
+        animator = GetComponentInChildren<Animator>();
+        timer = movementCooldown;
+    }
+
+    private void Update() {
+        if(timer < 0) {
+            Debug.Log("trigger");
+            animator.SetTrigger("movement");
+            timer += movementCooldown;
+        } else {
+            timer -= Time.deltaTime;
+        }
+
+    }
+
+}
